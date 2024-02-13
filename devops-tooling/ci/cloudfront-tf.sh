573ea905c2d32aa85da8046adb680ffe8b07479f
Chad Beaulac <chad.beaulac@inclinevillagetech.com>
2024-02-13 09:23:36 -0800

    Test cloudfront jest test in Github Action

---
+++ devops-tooling/ci/cloudfront-tf.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Setup NVM and Node.js
+. /usr/local/nvm/nvm.sh use 20
+
+# Create AWS config/credentials
+make setup-aws
+
+export AWS_PROFILE=localstack
+export AWS_CONFIG_FILE=/root/.aws/config
+export AWS_SHARED_CREDENTIALS_FILE=/root/.aws/credentials
+
+# The endpoint is not getting picked up from the profile in the config file.
+export AWS_ENDPOINT_URL="http://localhost.localstack.cloud:4566"
+
+# Setup Terraform stacks
+make local-tf-cfs3-init
+make local-tf-cfs3-plan
+make local-tf-cfs3-apply
+make local-tf-cfs3-output
+# Test Terraform stacks
+make local-tf-cfs3-test
+
+curl -X POST \
+     -H "Content-Type: application/json" \
+     -d '{"action": "kill"}' \
+     http://localhost.localstack.cloud:4566/_localstack/health
