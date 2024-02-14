339ce4790b74d77f4af4bfb7e760c68475d83c59
Chad Beaulac <chad.beaulac@inclinevillagetech.com>
2024-02-13 16:25:03 -0800

    installing puppeteer dependencies in github action test container

--- devops-tooling/ci/setup.sh
+++ devops-tooling/ci/setup.sh
@@ -23,6 +23,45 @@ apt-get update && apt-get install -y \
   gnupg2 \
   jq
 
+# Install puppeteer dependencies
+apt-get install -y \
+ca-certificates \
+fonts-liberation \
+libasound2 \
+libatk-bridge2.0-0 \
+libatk1.0-0 \
+libc6 \
+libcairo2 \
+libcups2 \
+libdbus-1-3 \
+libexpat1 \
+libfontconfig1 \
+libgbm1 \
+libgcc1 \
+libglib2.0-0 \
+libgtk-3-0 \
+libnspr4 \
+libnss3 \
+libpango-1.0-0 \
+libpangocairo-1.0-0 \
+libstdc++6 \
+libx11-6 \
+libx11-xcb1 \
+libxcb1 \
+libxcomposite1 \
+libxcursor1 \
+libxdamage1 \
+libxext6 \
+libxfixes3 \
+libxi6 \
+libxrandr2 \
+libxrender1 \
+libxss1 \
+libxtst6 \
+lsb-release \
+wget \
+xdg-utils
+
 
 # Setup NVM and Node.js
 . /usr/local/nvm/nvm.sh use 20
